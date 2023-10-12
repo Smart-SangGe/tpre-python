d92c0063d1a3c8b711f6c270352fe86d0c992d2c
dqy <1016751306@qq.com>
2023-10-12 20:50:00 +0800

    test: test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from gmssl import * #pylint: disable = e0401 
 
 sm3 = Sm3() #pylint: disable = e0602
-sm3.update(b'abc')
+sm3.update(b'1456456')
 dgst = sm3.digest()
-print("sm3('abc') : " + dgst.hex())
+print("sm3('111') : " + dgst.hex())
